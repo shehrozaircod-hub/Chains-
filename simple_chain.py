cd1313ba4592701d091e0c5130f602e3d6b8229a
shehroz.mir <shehroz.mir@aircod.com>
2025-11-07 10:01:06 +0500

    Built a Simple Chain

---
+++ simple_chain.py
@@ -0,0 +1,26 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Build the prompt template
+prompt = PromptTemplate(
+    template="Give five intersting facts about {topic}",
+    input_variables=["topic"]
+)
+
+def get_topic():
+    input_topic = input("Enter a topic: ")
+    return {"topic": input_topic}
+
+model = ChatOpenAI(model="gpt-5")
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke(get_topic())
+
+print(result)
